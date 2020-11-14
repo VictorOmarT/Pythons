07c7536d8b0941b504486794bce8b826f5177972
VictorOmarT.M <33044106+VictorOmarT@users.noreply.github.com>
2020-11-13 18:25:38 -0600

    Ejercicios.

---
+++ Lists.py
@@ -0,0 +1,31 @@
+"""list1 = range(2, 20, 2)
+list1_len = len(list1)
+print(list1_len)
+print(list(list1))"""
+
+
+"""coms = ["lilu", "yiyi", "tacha"]
+cun = ["oso, daruma, hanna"]
+coms.extend(cun)
+
+employees = ['Michael', 'Dwight', 'Jim', 'Pam', 'Ryan', 'Andy', 'Robert']
+index4 = employees [4]
+print(len(employees))
+print(employees[5])"""
+
+
+"""shopping_list = ['eggs', 'butter', 'milk', 'cucumbers', 'juice', 'cereal']
+print(len(shopping_list))
+last_element = shopping_list [-1]
+element5 = shopping_list [5]
+print(last_element, element5)"""
+
+
+"""suitcase = ['shirt', 'shirt', 'pants', 'pants', 'pajamas', 'books']
+
+beginning = suitcase[0:4]
+print(beginning)
+middle = suitcase[2:4]"""
+
+
+
